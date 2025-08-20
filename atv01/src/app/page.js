50a5d00714f5ae9ed70c0a60b72e892fd8807174
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-08-20 11:48:25 -0300

    colocando o titulo proposto pela atividade

--- atv01/src/app/page.js
+++ atv01/src/app/page.js
@@ -3,6 +3,10 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    
+    <div>
+      <h1 style={{ textAlign: "center", marginTop: "20px" }}>
+        Site criado para a disciplina Programação Front-End
+      </h1>
+    </div>
   );
 }
