fbcc61d1e29bb9d76e901ae439e61e78a410e6ca
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-08-20 12:04:59 -0300

    alterações no style do arquivo Profile.js

--- atv01/src/app/components/Profile.js
+++ atv01/src/app/components/Profile.js
@@ -6,9 +6,10 @@ const Profile = () => {
       <img 
         src="/profile.jpeg" 
         alt="Minha Foto" 
-        style={{ width: "150px", borderRadius: "50%" }} 
+        style={{ width: "400px"}} 
       />
       <h2>Marielly de Araújo Silva</h2>
+      <br></br>
       <p>Sou dev em formação e adoro programar em Java!</p>
       <p>Estou aprendendo React e Next.js para construir interfaces modernas.</p>
     </div>
