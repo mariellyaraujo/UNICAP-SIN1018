d89220dadc80625bd64f358211d3916b046f8ac0
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-11-06 19:11:59 -0300

    add conteudo na página

---
+++ atv03-portifolio/app/experiencia-academica/page.js
@@ -0,0 +1,18 @@
+export default function ExperienciaAcademica() {
+  return (
+    <div className="max-w-3xl mx-auto text-center">
+      <h1 className="text-4xl font-bold mb-6 text-[#ffbbd8]">
+        Experiência Acadêmica
+      </h1>
+      <p className="text-lg leading-relaxed text-[#ffbbd8]">
+        🎮 Formada em <span className="text-[#8e0000] font-semibold">Programação de Jogos Digitais</span> na
+        <span className="text-[#8e0000] font-semibold"> ETE Cícero Dias</span>.
+        <br />
+        💻 Atualmente cursando o Tecnólogo em
+        <span className="text-[#8e0000] font-semibold"> Sistemas para Internet</span> na
+        <span className="text-[#8e0000] font-semibold"> UNICAP</span>,
+        com bolsa do programa <span className="text-[#8e0000] font-semibold">Embarque Digital</span>.
+      </p>
+    </div>
+  );
+}
