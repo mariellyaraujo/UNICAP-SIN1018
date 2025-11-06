d438f79afd8343adb1405384f4a6276f35c6cd87
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-11-06 19:48:15 -0300

    add fade out na imagem de gatinho

--- atv03-portifolio/app/page.js
+++ atv03-portifolio/app/page.js
@@ -1,33 +1,46 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Home() {
   const [catImage, setCatImage] = useState(null);
+  const [fadeOut, setFadeOut] = useState(false);
 
   const fetchCat = async () => {
     const res = await fetch("https://api.thecatapi.com/v1/images/search");
     const data = await res.json();
     setCatImage(data[0].url);
+    setFadeOut(false); 
   };
+  useEffect(() => {
+    if (catImage) {
+      const timeout = setTimeout(() => setFadeOut(true), 1000); // 1s antes de desaparecer
+      const removeTimeout = setTimeout(() => setCatImage(null), 2000); // remove da tela
+      return () => {
+        clearTimeout(timeout);
+        clearTimeout(removeTimeout);
+      };
+    }
+  }, [catImage]);
 
   return (
-    <div className="flex flex-col items-center justify-center text-center min-h-screen">
+    <div className="flex flex-col items-center justify-center text-center min-h-screen bg-black">
       <h1 className="text-5xl md:text-7xl font-bold text-[#ffbbd8] mb-4 font-horizon">
         Marielly de Araújo Silva
       </h1>
       <p className="text-[#ffbbd8] text-lg mb-12">@dev</p>
 
-    <img
+      <img
         src="https://i.ibb.co/7xCSSRKS/mellyzinha.png"
         className="w-64 h-64 md:w-80 md:h-80 object-cover"
       />
-
 
       {catImage && (
         <img
           src={catImage}
           alt="Gatinho fofo"
-          className="w-64 h-64 object-cover rounded-2xl mb-8 border-2 border-[#8e0000] shadow-lg"
+          className={`w-64 h-64 object-cover rounded-full mb-8 border-2 border-[#ffbbd8] shadow-lg transition-opacity duration-1000 ${
+            fadeOut ? "opacity-0" : "opacity-100"
+          }`}
         />
       )}
 
