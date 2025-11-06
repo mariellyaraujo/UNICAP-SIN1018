2f582040c80f7d9eafae5edbc68407c1dee73973
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-11-06 19:53:44 -0300

    update segundos do fade out

--- atv03-portifolio/app/page.js
+++ atv03-portifolio/app/page.js
@@ -14,8 +14,8 @@ export default function Home() {
 
   useEffect(() => {
     if (catImage) {
-      const timeout = setTimeout(() => setFadeOut(true), 1000); 
-      const removeTimeout = setTimeout(() => setCatImage(null), 2000); 
+      const timeout = setTimeout(() => setFadeOut(true), 2500); 
+      const removeTimeout = setTimeout(() => setCatImage(null), 3000); 
       return () => {
         clearTimeout(timeout);
         clearTimeout(removeTimeout);
