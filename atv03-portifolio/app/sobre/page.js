38d63074033608409377f12f2eb16ed570ea9e12
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-11-10 19:14:48 -0300

    update estilo da pagina

--- atv03-portifolio/app/sobre/page.js
+++ atv03-portifolio/app/sobre/page.js
@@ -1,20 +1,37 @@
 export default function Sobre() {
   return (
-    <div className="max-w-3xl mx-auto text-center">
-      <h1 className="text-4xl font-bold mb-6 text-[#ffbbd8]">Sobre</h1>
-      <p className="text-[#ffbbd8] leading-relaxed text-lg">
-        Este portfólio foi desenvolvido com foco em um design minimalista e
-        elegante, utilizando as tecnologias <span className="text-[#8e0000] font-semibold">React</span> e <span className="text-[#8e0000] font-semibold">Next.js</span>.
-        <br />
-        A estilização foi feita com <span className="text-[#8e0000] font-semibold">Tailwind CSS</span>, garantindo responsividade e agilidade no
-        desenvolvimento.
-        <br />
-        Além disso, há integração com uma API pública, utilizando
-        <span className="text-[#8e0000] font-semibold"> TheCatAPI </span> para
-        exibir imagens aleatórias de gatos 🐱.
-        <br />
-        A hospedagem é feita na <span className="text-[#8e0000] font-semibold">Vercel</span> e o código está disponível no GitHub.
-      </p>
+    <div className="flex flex-col md:flex-row items-center justify-center min-h-screen px-6 md:px-12 gap-10">
+      <div className="flex-shrink-0">
+        <img
+          src="https://i.ibb.co/Q7JcPRbX/mellyzinha-pixel.png"
+          alt="Foto de Marielly"
+          className="w-64 h-64 md:w-80 md:h-80 object-cover rounded-3xl shadow-lg"
+        />
+      </div>
+
+      <div className="max-w-2xl text-left">
+        <h1 className="text-5xl md:text-6xl font-bold mb-6 text-[#ffbbd8]">
+          Sobre
+        </h1>
+        <p className="text-[#ffbbd8] leading-relaxed text-lg">
+          Este portfólio foi desenvolvido com foco em um design minimalista e
+          elegante, utilizando as tecnologias{" "}
+          <span className="text-[#8e0000] font-semibold">React</span> e{" "}
+          <span className="text-[#8e0000] font-semibold">Next.js</span>.
+          <br />
+          A estilização foi feita com{" "}
+          <span className="text-[#8e0000] font-semibold">Tailwind CSS</span>,
+          garantindo responsividade e agilidade no desenvolvimento.
+          <br />
+          Além disso, há integração com uma API pública, utilizando{" "}
+          <span className="text-[#8e0000] font-semibold">TheCatAPI</span> para
+          exibir imagens aleatórias de gatos 🐱.
+          <br />
+          A hospedagem é feita na{" "}
+          <span className="text-[#8e0000] font-semibold">Vercel</span> e o
+          código está disponível no GitHub.
+        </p>
+      </div>
     </div>
   );
 }
