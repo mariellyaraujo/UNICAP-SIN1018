a850bbda03e3e4e12aa768d0e31e3f7f7e6cb639
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-11-10 22:30:13 -0300

    update add hover ims

--- atv03-portifolio/app/projetos/page.js
+++ atv03-portifolio/app/projetos/page.js
@@ -1,15 +1,27 @@
+"use client";
+import { useState } from "react";
+
 export default function Projetos() {
+  const [hovered, setHovered] = useState("");
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen px-8 space-y-24">
-      <h1 className="text-6xl font-bold text-[#ffbbd8] mb-10 text-center">
+      <h1 className="text-6xl font-bold text-[#ffbbd8] mb-16 text-center">
         Projetos Desenvolvidos
       </h1>
 
+
       <div className="max-w-4xl w-full flex flex-col md:flex-row items-center justify-center gap-10">
         <img
-          src="https://i.ibb.co/Y47y9T3T/Design-sem-nome-7.png"
+          src={
+            hovered === "carteirou"
+              ? "https://i.ibb.co/B2JK6fXb/Design-sem-nome-9.png"
+              : "https://i.ibb.co/Y47y9T3T/Design-sem-nome-7.png"
+          }
           alt="Carteirou"
-          className="w-72 h-72 object-contain rounded-2xl"
+          onMouseEnter={() => setHovered("carteirou")}
+          onMouseLeave={() => setHovered("")}
+          className="w-80 h-80 object-contain rounded-2xl transition-transform duration-300 hover:scale-105"
         />
         <div className="text-left max-w-md">
           <h2 className="text-3xl font-bold text-white mb-2">Carteirou</h2>
@@ -19,11 +31,18 @@ export default function Projetos() {
         </div>
       </div>
 
+    
       <div className="max-w-4xl w-full flex flex-col md:flex-row-reverse items-center justify-center gap-10">
         <img
-          src="https://i.ibb.co/4RT2Th76/Design-sem-nome-6.png"
+          src={
+            hovered === "constela"
+              ? "https://i.ibb.co/N6dLLTc1/Design-sem-nome-8.png"
+              : "https://i.ibb.co/4RT2Th76/Design-sem-nome-6.png"
+          }
           alt="Projeto Constela"
-          className="w-72 h-72 object-contain rounded-2xl"
+          onMouseEnter={() => setHovered("constela")}
+          onMouseLeave={() => setHovered("")}
+          className="w-80 h-80 object-contain rounded-2xl transition-transform duration-300 hover:scale-105"
         />
         <div className="text-right max-w-md">
           <h2 className="text-3xl font-bold text-white mb-2">Projeto Constela</h2>
@@ -35,11 +54,18 @@ export default function Projetos() {
         </div>
       </div>
 
+     
       <div className="max-w-4xl w-full flex flex-col md:flex-row items-center justify-center gap-10">
         <img
-          src="https://i.ibb.co/Hpntjhy5/Design-sem-nome-5.png"
+          src={
+            hovered === "foliaverso"
+              ? "https://i.ibb.co/399DBYBy/Design-sem-nome-10.png"
+              : "https://i.ibb.co/Hpntjhy5/Design-sem-nome-5.png"
+          }
           alt="Foliaverso"
-          className="w-72 h-72 object-contain rounded-2xl"
+          onMouseEnter={() => setHovered("foliaverso")}
+          onMouseLeave={() => setHovered("")}
+          className="w-80 h-80 object-contain rounded-2xl transition-transform duration-300 hover:scale-105"
         />
         <div className="text-left max-w-md">
           <h2 className="text-3xl font-bold text-white mb-2">Foliaverso</h2>
