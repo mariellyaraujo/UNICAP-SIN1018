6c0221bc0f7e12f403f92e3733aea3aae10cba97
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-11-10 22:25:11 -0300

    update hover img

--- atv03-portifolio/app/experiencia-profissional/page.js
+++ atv03-portifolio/app/experiencia-profissional/page.js
@@ -1,14 +1,30 @@
+"use client";
+import { useState } from "react";
+
 export default function ExperienciaProfissional() {
+  const [hoverDream, setHoverDream] = useState(false);
+
   return (
     <div className="flex items-center justify-center min-h-screen px-6">
       <div className="max-w-4xl w-full flex flex-col md:flex-row items-center md:items-start gap-10">
-        <img
-          src="https://i.ibb.co/tTtdRgjP/Design-sem-nome-2.png"
-          alt="Dream Experience"
-          className="w-64 h-64 object-contain rounded-full"
-        />
+        <div
+          className="transition-transform duration-300 hover:scale-105"
+          onMouseEnter={() => setHoverDream(true)}
+          onMouseLeave={() => setHoverDream(false)}
+        >
+          <img
+            src={
+              hoverDream
+                ? "https://i.ibb.co/QvGVZxJ2/dreamhover.png"
+                : "https://i.ibb.co/tTtdRgjP/Design-sem-nome-2.png"
+            }
+            alt="Dream Experience"
+            className="w-64 h-64 object-contain rounded-full shadow-md transition-all duration-500"
+          />
+        </div>
+
         <div className="text-left">
-          <h1 className="text-5xl font-bold mb-6 text-[#ffbbd8]">
+          <h1 className="text-5xl md:text-6xl font-bold mb-6 text-[#ffbbd8]">
             Experiência Profissional
           </h1>
           <p className="text-lg text-[#ffbbd8] leading-relaxed">
@@ -16,8 +32,8 @@ export default function ExperienciaProfissional() {
             <span className="text-white font-semibold">
               Analista Desenvolvedora Salesforce
             </span>{" "}
-            na
-            <span className="text-white font-semibold"> Dream Experience</span>.
+            na{" "}
+            <span className="text-white font-semibold">Dream Experience</span>.
           </p>
         </div>
       </div>
